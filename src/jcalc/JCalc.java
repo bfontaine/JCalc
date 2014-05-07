8d61efa2a2f6855dc4a63299fff39eefc7c847f2
Baptiste Fontaine <batifon@yahoo.fr>
2014-05-07 21:53:13 +0200

    base GUI app

--- src/jcalc/JCalc.java
+++ src/jcalc/JCalc.java
@@ -1,7 +1,27 @@
 package jcalc;
 
-public class JCalc {
+import javax.swing.*;
+
+public class JCalc implements Runnable {
+    private static final String name = "JCalc";
+
+    public void run() {
+        JFrame jf = new JFrame(name);
+        JTextField result = new JTextField(20);
+
+        result.setText("0");
+        result.setEnabled(false);
+        result.setHorizontalAlignment(JTextField.RIGHT);
+
+        jf.add(result);
+        jf.pack();
+
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setVisible(true);
+    }
+
+
     public static void main (String[] args) {
-        System.out.println("Hello");
+        SwingUtilities.invokeLater(new JCalc());
     }
 }
