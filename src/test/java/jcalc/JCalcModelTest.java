3b5558f02507cbf7d788bf84e83085827ae7a9be
Baptiste Fontaine <batifon@yahoo.fr>
2014-05-10 17:18:47 +0200

    more JCalcModel tests

--- src/test/java/jcalc/JCalcModelTest.java
+++ src/test/java/jcalc/JCalcModelTest.java
@@ -18,6 +18,35 @@ public class JCalcModelTest {
 
     @Test
     public void testInitialValueIsZero() throws Exception {
-        assertEquals(0.0, m.getValue(), 0.0001);
+        assertEquals(0.0, m.getValue(), 0);
+    }
+
+    @Test
+    public void testSetValueChangesTheValue() throws Exception {
+        double val = 42;
+        m.setValue(val);
+        assertEquals(val, m.getValue(), 0);
+    }
+
+    @Test
+    public void testAppendDigit0To0() throws Exception {
+        m.appendDigit(0);
+        assertEquals(0, m.getValue(), 0);
+    }
+
+    @Test
+    public void testAppendDigit0ToNonZeroValue() throws Exception {
+        m.setValue(3);
+        m.appendDigit(0);
+        assertEquals(30, m.getValue(), 0);
+    }
+
+    @Test
+    public void testAppendDigit() throws Exception {
+        m.setValue(1);
+        m.appendDigit(2);
+        m.appendDigit(3);
+        m.appendDigit(4);
+        assertEquals(1234, m.getValue(), 0);
     }
 }
