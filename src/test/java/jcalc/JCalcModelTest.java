4ee00571c6f506c1bb264afc4b69276771c15a7b
Baptiste Fontaine <batifon@yahoo.fr>
2014-05-10 17:27:07 +0200

    missing JCalcModel tests added

--- src/test/java/jcalc/JCalcModelTest.java
+++ src/test/java/jcalc/JCalcModelTest.java
@@ -49,4 +49,84 @@ public class JCalcModelTest {
         m.appendDigit(4);
         assertEquals(1234, m.getValue(), 0);
     }
+
+    @Test
+    public void testPushSetTheValueToZero() throws Exception {
+        m.setValue(304);
+        m.push();
+        assertEquals(0, m.getValue(), 0);
+    }
+
+    @Test
+    public void testPopPreserveValue() throws Exception {
+        double val = 25;
+        m.setValue(42);
+        m.push();
+        m.setValue(val);
+        m.popValue();
+        assertEquals(val, m.getValue(), 0);
+    }
+
+    @Test
+    public void testPushPopSameValue() throws Exception {
+        double val = 25;
+        m.setValue(val);
+        m.push();
+        m.setValue(42);
+        assertEquals(val, m.popValue(), 0);
+    }
+
+    @Test
+    public void testPopEmptyGives0() throws Exception {
+        double top = 1395;
+        m.setValue(top);
+        m.push();
+        m.setValue(42);
+        assertEquals(top, m.popValue(), 0);
+        assertEquals(0, m.popValue(), 0);
+        assertEquals(0, m.popValue(), 0);
+    }
+
+    @Test
+    public void testResetValue() throws Exception {
+        m.setValue(173);
+        m.reset();
+        assertEquals(0, m.getValue(), 0);
+    }
+
+    @Test
+    public void testResetEmptyTheStack() throws Exception {
+        m.setValue(173);
+        m.push();
+        m.reset();
+        assertEquals(0, m.popValue(), 0);
+    }
+
+    @Test
+    public void testExecuteAdditionOnEmptyStack() throws Exception {
+        m.executeOperation('+');
+        assertEquals(0, m.getValue(), 0);
+    }
+
+    @Test
+    public void testExecuteAddition() throws Exception {
+        double a = 12,
+               b = 16;
+        m.setValue(a);
+        m.push();
+        m.setValue(b);
+        m.executeOperation('+');
+        assertEquals(a+b, m.getValue(), 0);
+    }
+
+    @Test
+    public void testExecuteSubtraction() throws Exception {
+        double a = 12,
+               b = 16;
+        m.setValue(a);
+        m.push();
+        m.setValue(b);
+        m.executeOperation('-');
+        assertEquals(a-b, m.getValue(), 0);
+    }
 }
